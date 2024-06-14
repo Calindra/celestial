71ba6b7fabef0d0fd64416ecdb79cd61634ddb73
sandhilt <6170125+sandhilt@users.noreply.github.com>
2024-06-14 04:29:05 -0300

    chore: Add Docker and Git ignore files for backend and frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,50 @@
-import { useState } from "react"
+import { useAccount, useConnect, useDisconnect } from "wagmi"
 import './App.css'
 import celestia from "./assets/celestia.svg"
 import cartesi from "./assets/cartesi.svg"
 
 function App() {
+  console.log("App")
+
+  const account = useAccount()
+  const { connectors, connect, status, error } = useConnect()
+  const { disconnect } = useDisconnect()
+
+  // const { data, error, status } = useEnsName({
+  //   address,
+  // })
+
+  console.log("account", account)
 
   return (
     <>
-      <section>
-        <h1 className="text-3xl font-bold underline">
-          Celestia 2 Cartesi
-        </h1>
+      <h1 className="text-3xl font-bold underline">
+           Celestia 2 Cartesi
+         </h1>
+      <h2>Account:</h2>
+      <div>
+        <p>status: {account.status}</p>
+        <p>addresses: {JSON.stringify(account.addresses)}</p>
+        <p>chainId: {account.chainId}</p>
+        {account.status === "connected" && (
+          <button type="button" onClick={() => disconnect()}>Disconnect</button>
+        )}
+      </div>
+      <div>
+        <h2>Connect</h2>
+        {connectors.map((connector) => (
+          <button key={connector.uid} type="button" onClick={() => connect({ connector })}>
+            {connector.name}
+          </button>
+        ))}
+        <div>Status: {status}</div>
+        <div>Error: {error?.message}</div>
+
         <div id="logos" className="flex-1">
-          <img className="logo object-cover" src={celestia} />
-          <img className="logo object-cover" src={cartesi} />
-        </div>
-        </section>
+           <img className="logo object-cover" src={celestia} />
+           <img className="logo object-cover" src={cartesi} />
+         </div>
+      </div>
     </>
   )
 }
---
+++ frontend/src/config.ts
@@ -0,0 +1,10 @@
+import { http, createConfig } from 'wagmi'
+import { anvil } from 'wagmi/chains'
+
+export const config = createConfig({
+  chains: [anvil],
+  transports: {
+    [anvil.id]: http(),
+    // [sepolia.id]: http(),
+  },
+})
