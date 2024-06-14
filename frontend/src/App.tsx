dbf21225017d6dd1ba482b851490c42ed07a69b0
sandhilt <6170125+sandhilt@users.noreply.github.com>
2024-06-14 05:58:52 -0300

    feat: more changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,47 @@
-import { useAccount, useConnect, useDisconnect } from "wagmi"
+import { useAccount, useConnect, useDisconnect, useWriteContract } from "wagmi"
 import './App.css'
 import celestia from "./assets/celestia.svg"
 import cartesi from "./assets/cartesi.svg"
+import { inputBoxAbi } from "./rollups"
+import type { MouseEventHandler } from "react"
+import { stringToHex } from "viem"
+// import { InputBox__factory } from "@cartesi/rollups"
+
+const INPUT_BOX_ADDR = "0x58Df21fE097d4bE5dCf61e01d9ea3f6B81c2E1dB"
+const DAPP_ADDR = "0xab7528bb862fb57e8a2bcd567a2e929a0be56a5e"
 
 function App() {
-  console.log("App")
-
   const account = useAccount()
   const { connectors, connect, status, error } = useConnect()
   const { disconnect } = useDisconnect()
+  const { writeContract } = useWriteContract();
 
-  // const { data, error, status } = useEnsName({
-  //   address,
-  // })
 
-  console.log("account", account)
+  // const callInputbox = async (payhex: `0x${string}`) => {
+  //   const ipt = InputBox__factory.connect(inputBoxAddress);
+  //   const result = await ipt.addInput(DAPP_ADDR, payhex);
+  //   console.log({ result });
+  // }
+
+  const handleInputBox: MouseEventHandler = (event ) => {
+    event.preventDefault();
+
+    const payload = {
+      hello: "celestia"
+    }
+
+    const payhex = stringToHex(JSON.stringify(payload));
+
+    console.log("Payload", {payload, payhex})
+
+    // callInputbox(payhex).catch(console.error)
+    writeContract({
+      abi: inputBoxAbi,
+      address: INPUT_BOX_ADDR,
+      functionName: "addInput",
+      args: [DAPP_ADDR, payhex],
+    })
+  }
 
   return (
     <>
@@ -31,7 +58,7 @@ function App() {
         )}
       </div>
       <div>
-        <h2>Connect</h2>
+        <h2 className="text-2xl font-bold underline">Connect</h2>
         {connectors.map((connector) => (
           <button key={connector.uid} type="button" onClick={() => connect({ connector })}>
             {connector.name}
@@ -39,6 +66,10 @@ function App() {
         ))}
         <div>Status: {status}</div>
         <div>Error: {error?.message}</div>
+
+        {account.isConnected && <div>
+          <button onClick={handleInputBox} type="button">Call inputbox</button>
+        </div>}
 
         <div id="logos" className="flex-1">
            <img className="logo object-cover" src={celestia} />
