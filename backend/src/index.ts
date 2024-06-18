a2c1446042825a66ded8411fa50e6dd91919fd70
sandhilt <6170125+sandhilt@users.noreply.github.com>
2024-06-17 22:03:34 -0300

    feat: Add Celestia API integration for fetching data

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,43 @@
+// XXX even though ethers is not used in the code below, it's very likely
+// it will be used by any DApp, so we are already including it here
 import createClient from "openapi-fetch";
 import { components, paths } from "./schema";
+import { decodeFunctionData } from "viem"
+import { Buffer } from "node:buffer"
+
+const celestiaAbi = [
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "namespace",
+        "type": "bytes29"
+      },
+      {
+        "name": "height",
+        "type": "uint256"
+      },
+      {
+        "name": "start",
+        "type": "uint256"
+      },
+      {
+        "name": "end",
+        "type": "uint256"
+      }
+    ],
+    "name": "CelestiaRequest",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bytes"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "pure",
+    "type": "function"
+  }
+] as const
 
 type AdvanceRequestData = components["schemas"]["Advance"];
 type InspectRequestData = components["schemas"]["Inspect"];
@@ -15,6 +53,52 @@ console.log("HTTP rollup_server url is " + rollupServer);
 
 const handleAdvance: AdvanceRequestHandler = async (data) => {
   console.log("Received advance request data " + JSON.stringify(data));
+
+  try {
+    const { POST } = createClient<paths>({ baseUrl: rollupServer });
+
+    const { args: [namespace, blockHeight, shareStart, shareEnd] } = decodeFunctionData({
+      abi: celestiaAbi,
+      data: data.payload
+    })
+
+    const gioID = "0x";
+
+    console.log({
+      namespace,
+      blockHeight,
+      shareStart,
+      shareEnd,
+      gioID
+    });
+
+    const { data: gioRes, error } = await POST("/gio", {
+      body: {
+        domain: 714,
+        id: gioID,
+      },
+      parseAs: "json",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (error || !gioRes) {
+      throw new Error(error ?? "Gio request failed");
+    }
+
+    const buffer = Buffer.from(gioRes.data, "hex");
+    const text = buffer.toString("utf-8");
+    const output = JSON.stringify({
+      ...gioRes,
+      text,
+    }, null, 4);
+    console.log("GioRes", output);
+
+  } catch (e) {
+    console.error("Error: " + e);
+  }
+
   return "accept";
 };
 
