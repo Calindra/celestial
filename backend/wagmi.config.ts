70a1716773387a79a7c164b702f93eea5df81ddb
sandhilt <6170125+sandhilt@users.noreply.github.com>
2024-06-17 23:38:09 -0300

    feat: Add CelestiaRelay contract ABI fetching and integration

--- backend/wagmi.config.ts
+++ backend/wagmi.config.ts
@@ -1,13 +1,33 @@
 import { defineConfig } from "@wagmi/cli"
 import hardhatDeploy from "@sunodo/wagmi-plugin-hardhat-deploy";
+import { fetch } from "@wagmi/cli/plugins"
 
 export default defineConfig({
     out: "src/rollups.ts",
     plugins: [
+        fetch({
+            contracts: [
+                {
+                    name: "CelestiaRelay",
+                }
+            ],
+            async parse({ response }) {
+                if (!response.ok) throw new Error(`Failed to fetch contract ABI: ${response.statusText}`)
+                const json = await response.json()
+                return json?.abi;
+            },
+            request(contract) {
+                const name = (contract as any).name;
+                return {
+                    url: `https://raw.githubusercontent.com/miltonjonat/rollups-celestia/main/onchain/deployments/sepolia/${name}.json`,
+                }
+            }
+        }),
         hardhatDeploy({
             directory: "../node_modules/@cartesi/rollups/export/abi",
             includes: [
-                /CartesiDApp/,
+                /IInputBox/,
+                /InputBox/,
             ]
         })
     ]
